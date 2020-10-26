6437337de2dc7b9c8eed655e527a8ec86b128ddb
Lauren Gifford <lauren.gifford.17@gmail.com>
2020-10-26 16:45:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function theBeatlesPlay(musicians, instruments){
   var musicianString;
   var musicianInstrument = []; 
   for (var i = 0; i< musicians.length; i++) {
-    
+    musicainString =
   }
   
   return musicianInstrument;
